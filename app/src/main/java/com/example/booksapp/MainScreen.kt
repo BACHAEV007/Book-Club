fd889507c8bc1e5eed444fe41869fbf00227dd20
mvbac <mv.bachaev@gmail.com>
2025-03-17 19:47:00 +0700

    Сделал кастомный BottomBar

--- app/src/main/java/com/example/booksapp/MainScreen.kt
+++ app/src/main/java/com/example/booksapp/MainScreen.kt
@@ -1,38 +1,157 @@
 package com.example.booksapp
 
+import android.annotation.SuppressLint
+import android.util.Log
 import androidx.collection.mutableObjectIntMapOf
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.absoluteOffset
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBars
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.NavGraph
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import com.example.booksapp.screen.LibraryScreen
+import com.example.booksapp.screen.SearchScreen
 import com.example.booksapp.screen.SignInScreen
 import com.example.booksapp.ui.LibraryScreenContent
 import com.example.booksapp.ui.SearchScreenContent
 import com.example.booksapp.ui.SignInScreenContent
+import com.example.booksapp.ui.component.CustomBottomBar
 
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun MainScreen(modifier: Modifier = Modifier, navController: NavHostController) {
-    NavHost(
-        navController = navController,
-        startDestination = SignInScreen
-    ){
-        composable<SignInScreen> {
-            SignInScreenContent(modifier = modifier.fillMaxSize(), {
-                navController.navigate(
-                    LibraryScreen
+    var selectedIndex by remember { mutableStateOf(0) }
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentRoute = navBackStackEntry?.destination?.route
+    Scaffold(
+        floatingActionButton = {
+            if (currentRoute in listOf(
+                    "com.example.booksapp.screen.LibraryScreen",
+                    "com.example.booksapp.screen.SearchScreen"
                 )
-            })
+            ) {
+                Box(
+                    modifier = Modifier
+                        .offset(y = 24.dp)
+                        .padding(horizontal = 16.dp)
+                        .windowInsetsPadding(WindowInsets.navigationBars)
+                ) {
+                    FloatingActionButton(
+                        onClick = {  },
+                        modifier = Modifier
+                            .size(80.dp)
+                            .align(Alignment.BottomCenter)
+                            .offset(y = 64.dp)
+                            .clip(RoundedCornerShape(64.dp)),
+                        containerColor = Color.Red
+                    ) {
+                        Icon(
+                            painter = painterResource(R.drawable.play_ic),
+                            contentDescription = null,
+                            tint = MaterialTheme.colorScheme.surface,
+                            modifier = Modifier.size(24.dp)
+                        )
+                    }
+                }
+            }
+        },
+        floatingActionButtonPosition = FabPosition.Center,
+        bottomBar = {
+            if (currentRoute in listOf(
+                    "com.example.booksapp.screen.LibraryScreen",
+                    "com.example.booksapp.screen.SearchScreen"
+                )
+            ) {
+                Box(
+                    modifier = Modifier
+                        .offset(
+                            y = -WindowInsets.navigationBars.asPaddingValues()
+                                .calculateBottomPadding()
+                        )
+                        .padding(horizontal = 16.dp)
+                        .windowInsetsPadding(WindowInsets.navigationBars)
+                ) {
+                    when (currentRoute) {
+                        "com.example.booksapp.screen.LibraryScreen",
+                        "com.example.booksapp.screen.SearchScreen",
+                            -> {
+                            CustomBottomBar(
+                                selectedIndex = selectedIndex,
+                                onItemSelected = { newIndex ->
+                                    selectedIndex = newIndex
+                                    when (newIndex) {
+                                        0 -> navController.navigate(LibraryScreen)
+                                        1 -> navController.navigate(SearchScreen)
+                                    }
+                                }
+                            )
+                        }
+                    }
+                }
+            }
         }
-        composable<LibraryScreen> {
-            SearchScreenContent(modifier = modifier.fillMaxSize().padding(horizontal = 16.dp))
+    ) {
+        Box(modifier = Modifier.fillMaxSize()) {
+            NavHost(
+                navController = navController,
+                startDestination = SignInScreen,
+                modifier = Modifier
+            ) {
+                composable<SignInScreen> {
+                    SignInScreenContent(modifier = modifier.fillMaxSize(), {
+                        navController.navigate(
+                            LibraryScreen
+                        )
+                    })
+                }
+                composable<LibraryScreen> {
+                    LibraryScreenContent(
+                        modifier = modifier
+                            .fillMaxSize()
+                            .padding(horizontal = 16.dp)
+                    )
+                }
+                composable<SearchScreen> {
+                    SearchScreenContent(
+                        modifier = modifier
+                            .fillMaxSize()
+                            .padding(horizontal = 16.dp)
+                    )
+                }
+            }
         }
     }
 }
---
+++ app/src/main/java/com/example/booksapp/ui/component/CustomBottomBar.kt
@@ -0,0 +1,93 @@
+package com.example.booksapp.ui.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.booksapp.R
+import kotlinx.serialization.Serializable
+
+@Composable
+fun CustomBottomBar(
+    selectedIndex: Int,
+    onItemSelected: (Int) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    BottomAppBar(
+        modifier = modifier
+            .fillMaxWidth()
+            .height(64.dp)
+            .clip(RoundedCornerShape(100)),
+        containerColor = MaterialTheme.colorScheme.primary,
+        contentPadding = PaddingValues(0.dp),
+        tonalElevation = 0.dp,
+        windowInsets = WindowInsets(0.dp)
+
+        ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceAround,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            val icons = listOf(
+                R.drawable.library_bottom_ic,
+                R.drawable.search_bottom_ic,
+                R.drawable.play_ic,
+                R.drawable.favorites_bottom_ic,
+                R.drawable.logout_bottom_ic
+            )
+
+            icons.forEachIndexed { index, iconRes ->
+                Box(
+                    contentAlignment = Alignment.Center,
+                    modifier = Modifier
+                        .weight(1f)
+                        .clickable { onItemSelected(index) },
+                ) {
+                    Icon(
+                        painter = painterResource(id = iconRes),
+                        contentDescription = null,
+                        tint = if (index == selectedIndex) MaterialTheme.colorScheme.surface else MaterialTheme.colorScheme.secondary,
+                        modifier = Modifier.size(24.dp)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun CustomBottomBarPreview() {
+    CustomBottomBar(selectedIndex = 0, onItemSelected = {})
+}
